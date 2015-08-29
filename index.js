29826ae873cb8c50a0eef9535ed876979a6501fc
Ubuntu <ubuntu@ip-172-31-21-244.us-west-2.compute.internal>
2015-08-29 06:39:18 +0000

    favicon working at port 80

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
   var express = require('express');
   var app = express();
   var path = require('path');
+  var favicon = require('serve-favicon');
   app.use(express.static('public'));
+  app.use(favicon(path.join(__dirname + '/public/favicon.ico')));
+
   
   app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/site.html'));
